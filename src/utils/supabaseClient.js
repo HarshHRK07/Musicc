1e3ebea2e14d58d21c4bb7e0befcd62ad0d504a0
HarshHRK07 <149139740+HarshHRK07@users.noreply.github.com>
2024-04-26 11:12:30 +0530

    Update supabaseClient.js

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'Your database url';
-const supabaseKey = 'Your database key';
+const supabaseUrl = 'https://vtczygoesepcyadfnpze.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ0Y3p5Z29lc2VwY3lhZGZucHplIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQxMTAwMzAsImV4cCI6MjAyOTY4NjAzMH0.INb61zL78wvVwSQydiG0eQqz9qVTlq5mKisqOEhUgDs';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
